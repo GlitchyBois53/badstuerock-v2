37cc84b149c68d6c0fbb0f832d68c28c127bf3c0
frbarbre <fr.barbre@gmail.com>
2023-06-06 17:04:46 +0200

    Co-authored-by: littleoldmanboy <littleoldmanboy@users.noreply.github.com> Co-authored-by: clickclickcode <clickclickcode@users.noreply.github.com>

---
+++ js/mapbox.js
@@ -0,0 +1,72 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZnJlZDk4OTEiLCJhIjoiY2xmYXh0bndwMmZ4YzN6bnRhOTZoa3BiZCJ9.ajDp70TNtFQ5XE4cKiArYQ";
+const map = new mapboxgl.Map({
+  container: "map",
+  // Replace YOUR_STYLE_URL with your style URL.
+  style: "mapbox://styles/fred9891/clik96ogp00em01pg5vb7gfr5",
+  center: [10.208778, 56.158807],
+  zoom: 16,
+});
+
+map.on("wheel", (event) => {
+  if (event.originalEvent.ctrlKey) {
+    return;
+  }
+
+  if (event.originalEvent.metaKey) {
+    return;
+  }
+
+  if (event.originalEvent.altKey) {
+    return;
+  }
+
+  event.preventDefault();
+});
+
+/* 
+Add an event listener that runs
+  when a user clicks on the map element.
+*/
+map.on("click", (event) => {
+  // If the user clicked on one of your markers, get its information.
+  const features = map.queryRenderedFeatures(event.point, {
+    layers: ["symbols"], // replace with your layer name
+  });
+  if (!features.length) {
+    return;
+  }
+  const feature = features[0];
+
+  /* 
+    Create a popup, specify its options 
+    and properties, and add it to the map.
+  */
+  const popup = new mapboxgl.Popup({ offset: [0, -25] })
+    .setLngLat(feature.geometry.coordinates)
+    .setHTML(
+    `
+      <div class="popup-card">
+        <h3 class="popup-header">BADSTUE<span>ROCK</span></h3>
+        <a href="https://goo.gl/maps/5w6DURsU4BGbTK7h6" target="_blank" class="popup-address" >Badstuegade 22</a>
+        <div class="popup-hours">
+            <h4>MAN - TOR</h4>
+            <h5>11:00-17:30</h5>
+        </div>
+        <div class="popup-hours">
+            <h4>FRE</h4>
+            <h5>11:00-18:00</h5>
+        </div>
+        <div class="popup-hours">
+            <h4>LØR</h4>
+            <h5>10:30-15:30</h5>
+        </div>
+        <div class="popup-hours">
+            <h4>SØN</h4>
+            <h5>LUKKET</h5>
+        </div>
+      </div>
+    `
+    )
+    .addTo(map);
+});
