ac2c00010b3125a8bfa8ae403cca00f02758a2e9
frbarbre <fr.barbre@gmail.com>
2023-06-13 11:21:54 +0200

    Co-authored-by: Lean Kenneth Hansen (EAALKH) <eaalkh@students.eaaa.dk> Co-authored-by: littleoldmanboy <littleoldmanboy@users.noreply.github.com>

--- js/mail.js
+++ js/mail.js
@@ -15,87 +15,61 @@ function sendMail() {
   var errorEmail = document.getElementById("error-email");
   var errorMessage = document.getElementById("error-textarea");
 
-  emailjs
-    .send(serviceID, templateID, params)
-    .then((res) => {
-      let count = 0;
+  let count = 0;
 
-      if (!params.from_name == "") {
-        count += 1;
-        errorName.classList.remove("error-active");
-        console.log(count + " Name success");
-      } else {
-        // Error
-        errorName.classList.add("error-active");
-        errorName.innerHTML = "type something in name";
-        console.log("type something in name");
-      }
-      // WE COULD BREAK UP THE IF-STATEMENT TO BE ABLE TO DELIVER ERROR MESSAGES FOR EVERY POSSIBLE ERROR
+  if (!params.from_name == "") {
+    count += 1;
+    errorName.classList.remove("error-active");
+    console.log(count + " Name success");
+  } else {
+    // Error
+    errorName.classList.add("error-active");
+    errorName.innerHTML = "type something in name";
+    console.log("type something in name");
+  }
+  // WE COULD BREAK UP THE IF-STATEMENT TO BE ABLE TO DELIVER ERROR MESSAGES FOR EVERY POSSIBLE ERROR
 
-      if (
-        params.from_email.includes("@") &&
-        params.from_email.includes(".") &&
-        !params.from_email.includes(" ")
-      ) {
-        count += 1;
-        errorEmail.classList.remove("error-active");
-        console.log(count + " Email success");
-      } else {
-        // Error
-        errorEmail.classList.add("error-active");
-        errorEmail.innerHTML = "Email must include @ '.' and no spaces";
-        console.log("enter valid email");
-      }
-      // if (params.from_email.includes("@")) {
-      //   count += 1
-      //   errorEmail.classList.remove("error-active")
-      //   console.log(count + " @ success")
-      // } else {
-      //   // Error
-      //   errorEmail.classList.add("error-active")
-      //   errorEmail.innerHTML += "@"
-      //   console.log("Must contain @")
-      // }
-      // // .
-      // if (params.from_email.includes(".")) {
-      //   count += 1
-      //   errorEmail.classList.remove("error-active")
-      //   console.log(count + " . success")
-      // } else {
-      //   // Error
-      //   errorEmail.classList.add("error-active")
-      //   errorEmail.innerHTML += "."
-      //   console.log("Must contain .")
-      // }
-      // // ' '
-      // if (!params.from_email.includes(" ")) {
-      //   count += 1
-      //   errorEmail.classList.remove("error-active")
-      //   console.log(count + " ' ' success")
-      // } else {
-      //   // Error
-      //   errorEmail.classList.add("error-active")
-      //   errorEmail.innerHTML += "' '"
-      //   console.log("Mustn't contain ' '")
-      // }
+  if (
+    params.from_email.includes("@") &&
+    params.from_email.includes(".") &&
+    !params.from_email.includes(" ")
+  ) {
+    count += 1;
+    errorEmail.classList.remove("error-active");
+    console.log(count + " Email success");
+  } else {
+    // Error
+    errorEmail.classList.add("error-active");
+    errorEmail.innerHTML = "Email must include @ '.' and no spaces";
+    console.log("enter valid email");
+  }
 
-      if (!params.message == "") {
-        count += 1;
-        errorMessage.classList.remove("error-active");
-        console.log(count + " Message success");
-      } else {
-        // Error
-        errorMessage.classList.add("error-active");
-        errorMessage.innerHTML = "Message can't be empty";
-        console.log("Message can't be empty");
-      }
+  if (!params.message == "") {
+    count += 1;
+    errorMessage.classList.remove("error-active");
+    console.log(count + " Message success");
+  } else {
+    // Error
+    errorMessage.classList.add("error-active");
+    errorMessage.innerHTML = "Message can't be empty";
+    console.log("Message can't be empty");
+  }
 
-      if (count === 3) {
-        console.log(count);
-        success();
-      }
-    })
-    .catch((err) => console.log(err));
+  if (count === 3) {
+    console.log(count);
+    success();
+    isSubmitted = true;
+
+    emailjs
+      .send(serviceID, templateID, params)
+      .then((res) => {
+        // Reset efter sendt form
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+      })
+      .catch((err) => console.log(err));
+  }
 }
 
 function closesuccess() {
@@ -104,27 +78,25 @@ function closesuccess() {
 }
 
 function success() {
-  document.getElementById("name").value = "";
-  document.getElementById("email").value = "";
-  document.getElementById("message").value = "";
+  // Pop-up der fortæller brugeren at mailen er sendt
   successWindow.classList.remove("success-not-active");
   successWindow.classList.add("success-active");
 }
 
-var wordCount = document.getElementById("word-count")
+var wordCount = document.getElementById("word-count");
 
 const textarea = document.getElementById("message");
 
 textarea.addEventListener("input", ({ currentTarget: target }) => {
   const maxLength = target.getAttribute("maxlength");
-  const currentLength = target.value.length;
-  
-  wordCount.classList.remove("word-count-max")
-  console.log(`max: ${maxLength}, current: ${currentLength}`)
+  let currentLength = target.value.length;
+
+  wordCount.classList.remove("word-count-max");
+  console.log(`max: ${maxLength}, current: ${currentLength}`);
 
   if (currentLength == maxLength) {
-    wordCount.classList.add("word-count-max")
+    wordCount.classList.add("word-count-max");
   }
 
-  wordCount.innerHTML = `${currentLength}/${parseInt(maxLength)}`;
+  wordCount.innerHTML = `${currentLength}/${maxLength}`;
 });
